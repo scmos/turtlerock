7980f82a566fc8e2844e9ec813dd69e5081ac87b
Oscar Sanchez V <oscar.scmos@gmail.com>
2020-07-18 13:15:38 -0500

    Initial Commit

---
+++ singleview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  singleview
+//
+//  Created by scmos on 7/18/20.
+//  Copyright © 2020 scmos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
